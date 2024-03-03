3186d7e8326ffc138c8d50db5ecfea7f4f3a780a
Etsri Klussey <etsri.klussey@eecc.de>
2024-03-03 20:56:40 +0100

    add RequestMapping

--- src/main/java/com/robic/address/controllers/AddressController.java
+++ src/main/java/com/robic/address/controllers/AddressController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("address")
+@RequestMapping("/address")
 public class AddressController {
 
     private final AddressService addressService;
